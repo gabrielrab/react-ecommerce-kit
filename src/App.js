45ec27a5b2cdf65882d7c9b74009f651394b7d65
Gabriel Rabelo Camargos <contato.ideialisa@gmail.com>
2020-07-15 23:55:01 -0300

    finish alerts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from './components/Layout';
+import Alert from './components/Alerts';
 
 function App() {
   return (
@@ -8,6 +9,21 @@ function App() {
         <h1>Header h1</h1>
         <h2>Header h2</h2>
         <h3>Header h3</h3>
+        <div>
+          <label>Alerts</label>
+          <Alert>Mensagem simples de alerta</Alert>
+          <Alert type="gray" align="center">
+            Mensagem simples de alerta
+          </Alert>
+          <Alert type="success" align="left">
+            Mensagem simples de alerta
+          </Alert>
+          <Alert type="danger" align="rigth">
+            Mensagem simples de alerta
+          </Alert>
+          <Alert type="warning">Mensagem simples de alerta</Alert>
+          <Alert type="ligth-green">Mensagem simples de alerta</Alert>
+        </div>
       </Layout>
     </>
   );
