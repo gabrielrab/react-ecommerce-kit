475412af3f67ec495d9d6f5fc39795e9d6f69281
Gabriel Rabelo Camargos <contato.ideialisa@gmail.com>
2020-07-15 22:50:03 -0300

    add layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './style.scss';
 
 const Layout = (props) => {
-  return <main>{props.children}</main>;
+  return (
+    <main>
+      <section>{props.children}</section>
+    </main>
+  );
 };
 
 export default Layout;
